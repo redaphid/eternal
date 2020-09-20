95fee3dccb4f9a07695baebcab07ea2fc5a9186f
Aaron redaphid Herres <aherres@magicleap.com>
2020-09-20 14:03:10 -0700

    env hints for DESTINATION_DISK

--- env.fish
+++ env.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 set -x SOURCE_DISK /dev/disk/by-id/nvme-Force_MP600_203182840001300527A3
-set -x TARGET_DISK /dev/zvol/rpool/safe-vm
+set -x DESTINATION_DISK /dev/zvol/rpool/safe-vm
 set -x SOURCE_DATASET charon/souls/laptop/rpool/ROOT/eternal
